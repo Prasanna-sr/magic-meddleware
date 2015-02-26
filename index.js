c161348c618a5347233863e424d4ca762abc6e73
Arick Chow <achow@ancestry.com>
2015-02-26 14:40:52 -0800

    added basic template

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var http = require('http'),
+    path = require('path'),
     express = require('express'),
     meddleware = require('meddleware'),
     config = require('shush')('./config/middleware');
 
 
 var app = express();
-
+app.engine('.html', require('ejs').__express);
+app.set('views', './views');
+app.set('view engine', 'html');
+app.use('/', express.static(path.join(__dirname, './public')));
 app.use(meddleware(config));
 
 var router = express.Router;
 
 app.get('/', function(req, res) {
   console.log("Cookies: ", req.cookies);
-  res.send('ok');
+  res.render('index', {config: config });
 });
 
 
@@ -20,4 +24,4 @@ app.get('/magic', function(req, res) {
   res.send('ok');
 });
 
-http.createServer(app).listen(8080);
+http.createServer(app).listen(3000);
