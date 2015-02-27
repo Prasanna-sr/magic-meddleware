da70fb72aab51dfc9c53f058c8a52bb65353fb5c
prasanna ramanujam <prasannaraajkumar@gmail.com>
2015-02-26 17:45:54 -0800

    initial working dynmaic middleware

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 var http = require('http'),
     express = require('express'),
-    meddleware = require('meddleware'),
-    config = require('shush')('./config/middleware');
+    meddleware = require('meddleware');
+
+var config = require('shush')('./config/middleware');
 
 
 var app = express();
 
+
 app.use(meddleware(config));
 
-var router = express.Router;
+var router = express.Router();
 
-app.get('/', function(req, res) {
-  console.log("Cookies: ", req.cookies);
-  res.send('ok');
+app.use(router);
+
+
+
+
+router.use(function(req, res, next) {
+	console.log('ok router1');
+	next();
 });
 
 
-app.get('/magic', function(req, res) {
-  res.send('ok');
+router.get('/', function(req, res) {
+    console.log("Cookies: ", req.cookies);
+    //console.log(app._router.stack);
+    res.send('ok');
+});
+
+router.get('/magic', function(req, res) {
+    //delete require.cache[require.resolve('./config/middleware')];
+    var config = require('shush')('./config/middleware');
+    config['prk'].changed = true;
+    app.use(meddleware(config, 1));
+    res.send('success');
 });
 
 http.createServer(app).listen(8080);
