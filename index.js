8514227f157f31ed583b310f252f1a31469c4b34
prasanna ramanujam <prasannaraajkumar@gmail.com>
2015-03-02 13:03:30 -0800

    demo commit

--- index.js
+++ index.js
@@ -1,53 +1,26 @@
-var http = require('http'),
-    path = require('path'),
-    express = require('express'),
-    meddleware = require('meddleware'),
-    bodyParser = require('body-parser');
+var express = require('express');
+var meddleware = require('meddleware');
+var bodyParser = require('body-parser');
 
 var config = require('shush')('./config/middleware');
 
 var app = express();
-app.engine('.html', require('ejs').__express);
-app.set('views', './views');
-app.set('view engine', 'html');
-app.use('/', express.static(path.join(__dirname, './public')));
+
 app.use(bodyParser.json());
+
 app.use(meddleware(config));
 
-
 var router = express.Router();
-
 app.use(router);
 
-
-router.use(function(req, res, next) {
-	console.log('ok default');
-	next();
+router.get('/', function(req, res) {
+    res.send('Test Page');
 });
-
 
 router.get('/cookie', function(req, res) {
-    console.log("Cookies: ", req.cookies);
-    //console.log(app._router.stack);
-    res.send('ok');
+    res.send(req.cookies);
 });
 
-// router.get('/magic', function(req, res) {
-//     //delete require.cache[require.resolve('./config/middleware')];
-//     var config = require('shush')('./config/middleware');
-//     config['prk'].changed = true;
-//     app.use(meddleware(config, 1));
-//     res.send('success');
-// });
-
-router.use(function(req, res, next) {
-	console.log('test after routes');
-	next();
+app.listen(3000, function() {
+    console.log('server running in port 3000');
 });
-
-router.get('/test', function(req, res) {
-	res.send('test');
-});
-
-
-http.createServer(app).listen(3000);
